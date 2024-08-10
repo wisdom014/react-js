5e278dc3a5d0c9874397fd1236c901dba6d97e77
wisdom014 <elijahakinniyi014@gmail.com>
2024-08-10 19:40:32 +0100

    updated

--- project1/main.js
+++ project1/main.js
@@ -1,11 +1,45 @@
-function MainContent() {
- return (
- <h1>Hi there!!</h1>
- )
-}
+// React app
+// function MainContent() {
+//  return (<h1>Hi there!!</h1>)
+// }
+// function BodyContent() {
+//  return (<p>How are you</p>)
+// }
 
-ReactDOM.render(
-  <MainContent /> 
-,
- document.getElementById("root")
-)
+// ReactDOM.render
+// (<div>
+//    <MainContent />
+//   <BodyContent />
+// </div>
+// ,
+//  document.getElementById("root")
+// )
+
+// let paragraph = document.getElementById("root");
+//  let node = document.createElement("h1");
+//  node.textContent = "Hello, React!";
+// paragraph.append(node);
+
+// function content() {
+//  return (
+//   paragraph.innerHTML = "<h2>Hello, React!!!</h2>"
+//  )
+// }
+// content();
+
+const head = <div>
+ <h1>Hello</h1>
+ <div>
+  <ul>
+   <li>one</li>
+   <li>two</li>
+   <li>three</li>
+  </ul>
+ </div>
+     <h2>Welcome to my website!</h2>
+   <p>I'm learning React.</p>
+</div>;
+
+
+    ReactDOM.render(
+     head, document.getElementById("root"))
