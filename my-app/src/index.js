983e983f0760f546ed18dcf307c060459ae1aa70
wisdom014 <elijahakinniyi014@gmail.com>
2024-08-12 11:53:35 +0100

    Greet

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 
-function Name() {
+function App() {
   const date = new Date()
   const hours = date.getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+  }
+  else if (hours >= 12 && hours <= 17) {
+   timeOfDay = "afternoon";
+  }
+  else {
+    timeOfDay = "night";
+  }
   return (
-    <h1> {hours} : {date.getMinutes()} : {date.getSeconds()}</h1>
+    <h1>Good {timeOfDay}!!</h1>
   )
 }
 
-ReactDOM.render(<Name />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
