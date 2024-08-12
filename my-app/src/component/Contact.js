a6cdbc7be242c0ed7773807a09956ba70e6abda6
wisdom014 <elijahakinniyi014@gmail.com>
2024-08-12 21:19:51 +0100

    destructed props

--- my-app/src/component/Contact.js
+++ my-app/src/component/Contact.js
@@ -1,14 +1,14 @@
 import React from "react";
-import logo from "../img/wallpaper.png"
-import "../index.css"
+import logo from "../img/wallpaper.png";
+import "../index.css";
 
-export default function Contact(props) {
+export default function Contact({name, number, email}) {
  return (
  <div className="contact-card">
   <img src={logo} className="contact-image" alt="card-img" />
-  <h3 className="contact-name">{props.name}</h3>
-  <p className="contact-number info">{props.number}</p>
-  <p className="contact-email info">{props.email}</p>
+  <h3 className="contact-name">{name}</h3>
+  <p className="contact-number info">{number}</p>
+  <p className="contact-email info">{email}</p>
  </div>
  )
 }
