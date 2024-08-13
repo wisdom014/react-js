2c1734bc55e08bac97720a869389e57c3543a912
wisdom014 <elijahakinniyi014@gmail.com>
2024-08-13 20:36:52 +0100

    mapping array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,30 +1,36 @@
 import React from "react";
-import Joke from "./component/Joke";
+// import Joke from "./component/Joke";
 
 
 export default function App() {
+  const colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Violet"]
+  const color = colors.map((colors) => {
+ return <h3>{colors}</h3>
+})
  return (
   <div>
-   <Joke
-    setup="I got my daughter a fridge for her birthday."
-    punchline="I can't wait to see her face light up when she opens it."
-    />
-   <Joke
-    setup="How did the hacker escape the police?"
-    punchline="He just ransomware!"
-    />
-   <Joke
-    setup="Why don't pirates travel on mountain roads?"
-    punchline="Scurvy."
-    />
-   <Joke
-    setup="Why do bees stay in the hive in the winter?"
-    punchline="Swarm."
-    />
-   <Joke
-    setup="What's the best thing about Switzerland?"
-    punchline="I don't know, but the flag is a big plus!"
-    />
+    {color}
   </div>
  )
 }
+
+  //  <Joke
+  //   setup="I got my daughter a fridge for her birthday."
+  //   punchline="I can't wait to see her face light up when she opens it."
+  //   />
+  //  <Joke
+  //   setup="How did the hacker escape the police?"
+  //   punchline="He just ransomware!"
+  //   />
+  //  <Joke
+  //   setup="Why don't pirates travel on mountain roads?"
+  //   punchline="Scurvy."
+  //   />
+  //  <Joke
+  //   setup="Why do bees stay in the hive in the winter?"
+  //   punchline="Swarm."
+  //   />
+  //  <Joke
+  //   setup="What's the best thing about Switzerland?"
+  //   punchline="I don't know, but the flag is a big plus!"
+  //   />
